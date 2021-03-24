f094d65f212a6cff48271b1e72eeb51e5f50cf0d
Pablo <Pablo@DESKTOP-DNHR1A7>
2021-03-24 11:34:06 +0100

    añadido leer numero

--- src/examen_24M_Primo/main.java
+++ src/examen_24M_Primo/main.java
@@ -1,14 +1,21 @@
 package examen_24M_Primo;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
+		
+		Scanner ent = new Scanner(System.in);
+		int num;
 		
 		System.out.println("Buenos dias Ceinmark,\n"
 				+ "soy Pablo Saturio Herrezuelo.\n"
 				+ "\n"
 				+ "Introduce un numero para averiguar si es primo o no:");
 
+		num = ent.nextInt();
+		
 	}
 
 }
